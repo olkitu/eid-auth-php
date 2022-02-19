5d9a33b07f6b82915fc8ad93a980936c0fd9f317
Minh Truong <minh@truong.fi>
2022-02-19 19:32:25 +0200

    Add CRLRevocationPath and download all VRK CRL's on every startup

--- README.md
+++ README.md
@@ -9,13 +9,15 @@ Support login with Estonian and Finnish ID cards
 
 Generate certificates
 
-````
+```
 DOMAIN_NAME=localhost
 openssl genrsa -out ./eid/server.key 2048 && 
 openssl req -new -key ./eid/server.key -out ./eid/server.csr -subj "/C=FI/ST=Uusimaa/L=Helsinki/O=Test/OU=Test Department/CN=$DOMAIN_NAME" && \
 openssl x509 -req -days 365 -in ./eid/server.csr -signkey ./eid/server.key -out ./eid/server.crt &&
 rm ./eid/server.csr
 ```
+
+Start container in compose
 
 ```
 docker-compose up -d --build
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# Download CRL lists
+curl -s http://proxy.fineid.fi/arl/vrkroota.crl -o /etc/ssl/eid/crl/vrkroota.crl
+curl -s http://proxy.fineid.fi/crl/dvvroot3ec.crl -o /etc/ssl/eid/crl/dvvroot3ec.crl
+curl -s http://proxy.fineid.fi/arl/vrkroot2a.crl -o /etc/ssl/eid/crl/vrkroot2a.crl
+curl -s http://proxy.fineid.fi/crl/dvvroot3rc.crl -o /etc/ssl/eid/crl/dvvroot3rc.crl
+
+cd /etc/ssl/eid/crl && for i in *.crl; do openssl crl -in $i -inform DER -out $i; ln -sf $i `openssl crl -noout -hash -in $i`.0; done
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
         set -- apache2-foreground "$@"
