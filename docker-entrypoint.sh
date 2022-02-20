bb4c95a2245aedcb8d772809e352b79b4fc5aed2
Minh Truong <minh@truong.fi>
2022-02-20 09:59:50 +0200

    Add Italian National ID Card support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,11 @@
 set -e
 
 cd /etc/ssl/eid/crl
-
+#
 # Download CRL lists
+#
+
+# https://dvv.fineid.fi/en/certificate-index
 curl -s https://dvv.fineid.fi/api/v1/cas/702/revocationlist -o vrkroot2c.crl
 curl -s https://dvv.fineid.fi/api/v1/cas/711/revocationlist -o dvvroot3rc.crl
 curl -s https://dvv.fineid.fi/api/v1/cas/712/revocationlist -o dvvroot3ec.crl
@@ -12,9 +15,12 @@ curl -s https://dvv.fineid.fi/api/v1/cas/102/revocationlist -o vrkcqc3c.crl
 curl -s https://dvv.fineid.fi/api/v1/cas/112/revocationlist -o dvvcqc4ec.crl
 curl -s https://dvv.fineid.fi/api/v1/cas/111/revocationlist -o dvvcqc4rc.crl
 
+# https://www.skidsolutions.eu/en/repository/CRL/
 curl -s http://c.sk.ee/EE-GovCA2018.crl -o EE-GovCA2018.crl
 
-
+# https://www.cartaidentita.interno.gov.it/fornitori-di-servizi/certification-autority/
+curl -s https://ldap.cie.interno.gov.it/ciesubca1.crl -o ciesubca1.crl
+curl -s https://ldap.cie.interno.gov.it/ciesubca002.crl -o ciesubca002.crl
 
 for i in *.crl; do openssl crl -in $i -inform DER -out $i; ln -sf $i `openssl crl -noout -hash -in $i`.r0; done
 
